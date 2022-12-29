15876a81af31423e0e31aa47330687ddfa47dfbe
yevagav <yevagavrilov@gmail.com>
2022-12-28 16:19:24 -0800

    installed server dep

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+require('dotenv').config()
+require('./config/database');
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
+app.use(express.json())
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
 app.use(logger('dev'));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.use('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`I am listening on ${PORT}`)
+})
---
+++ src/pages/App/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Yeva's List
+    </div>
+  );
+}
+
+export default App;
